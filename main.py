0e1f8ad1569dc4b415d70b3fb7f2a95917c59764
bradleykrafft <56617611+bradleykrafft@users.noreply.github.com>
2025-07-27 15:01:23 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import os
 import streamlit as st
 import qrcode
 from streamlit import empty, exception
-
 from pathlib import Path
 
 st.title("Easy QR")
@@ -12,7 +11,7 @@ save_file_as = st.text_input("Enter a name for the file.")
 
 # Generate and save the QR Code as an image
 if st.button("Generate QR code."):
-    st.warning(f"Generating QR code: {data} ")
+    st.write(f"Generating QR code: {data} ")
     try:
         qr = qrcode.QRCode(
             version=1,  # Controls the size of the QR Code
@@ -25,7 +24,8 @@ if st.button("Generate QR code."):
         img = qr.make_image(fill_color="black", back_color="white")
         if save_file_as == empty:
              filename = "MY_QRCode"
-        folder_path = str(os.path.join(Path.home(), "Downloads"))
+        folder_path = downloads_path = os.path.join(os.path.expanduser("~"), "Downloads")
+        #folder_path = str(os.path.join(Path.home(), "Downloads"))
         img.save(f"{folder_path}\\{save_file_as}.png")
     except Exception as e:
         print(e)
