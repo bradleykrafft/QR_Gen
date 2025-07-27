19b734c139328ac4cbff56ce75ba23aba1cc7ea3
bradleykrafft <56617611+bradleykrafft@users.noreply.github.com>
2025-07-27 14:31:20 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import os
 import streamlit as st
 import qrcode
-import wx
 from streamlit import empty, exception
-from wx.    py.editor import fileDialog
+
 from pathlib import Path
 
 st.title("Easy QR")
@@ -27,12 +26,6 @@ if st.button("Generate QR code."):
         if save_file_as == empty:
              filename = "MY_QRCode"
         folder_path = str(os.path.join(Path.home(), "Downloads"))
-        # app = wx.App(False)
-        # dialog = wx.DirDialog(None, "Select a folder:", style=wx.DD_DEFAULT_STYLE | wx.DD_NEW_DIR_BUTTON)
-        # if dialog.ShowModal() == wx.ID_OK:
-        #     folder_path = dialog.GetPath()
-        # st.write('Your file will be save to: ', folder_path)
-        # dialog.Destroy()
         img.save(f"{folder_path}\\{save_file_as}.png")
     except Exception as e:
         print(e)
