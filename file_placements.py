e45131fc33e62e2df89b9b81fd7481da34f68e3b
bradleykrafft <56617611+bradleykrafft@users.noreply.github.com>
2025-07-28 16:36:17 -0400

    Add files via upload

---
+++ file_placements.py
@@ -0,0 +1,39 @@
+import qrcode
+import wx
+import PIL
+import base64
+import io
+import os
+from pathlib import Path
+from PIL import Image
+
+
+def get_file_path():
+    app = wx.App(False)
+    frame = wx.Frame(None, -1, "Hidden Frame")  # Create a hidden frame for the dialog
+
+    # Create a directory dialog
+    dialog = wx.DirDialog(frame, "Choose a directory", style=wx.DD_DEFAULT_STYLE)
+
+    if dialog.ShowModal() == wx.ID_OK:  # If the user selects a directory
+        selected_directory = dialog.GetPath()  # Get the selected directory as a string
+
+        dialog.Destroy()  # Destroy the dialog to free resources
+        frame.Destroy()  # Destroy the hidden frame
+        return selected_directory
+
+
+def qr_code_gen(link : str, file_path, name_of_file : str):
+    selected_dir = file_path
+    normalized_path = Path(selected_dir)
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
+    qr.add_data(link)
+    qr.make(fit=True)
+    img = qr.make_image(fill_color="black", back_color="white")
+    img.save(f"{normalized_path}\\{name_of_file}.png")
+
--- main.py
+++ main.py
@@ -1,44 +1,21 @@
-import os
+
 import streamlit as st
-import qrcode
-from streamlit import empty, exception
-from pathlib import Path
+from file_placements import *
 
 st.title("Easy QR")
 
 data = st.text_input("Enter link to convert to QR code.")
 save_file_as = st.text_input("Enter a name for the file.")
+if 'file_location' not in st.session_state:
+    st.session_state['file_location'] = "C:\\Users\\public\\downloads"
 
-# Generate and save the QR Code as an image
-if st.button("Generate QR code."):
-    st.write(f"Generating QR code: {data} ")
-    try:
-        qr = qrcode.QRCode(
-            version=1,  # Controls the size of the QR Code
-            error_correction=qrcode.constants.ERROR_CORRECT_L,  # Error correction level
-            box_size=10,  # Size of each box in the QR code grid
-            border=4,  # Thickness of the border
-        )
-        qr.add_data(data)
-        qr.make(fit=True)
-        img = qr.make_image(fill_color="black", back_color="white")
-        if save_file_as == empty:
-             filename = "MY_QRCode"
-        folder_path = downloads_path = os.path.join(os.path.expanduser("~"), "Downloads")
-        #folder_path = str(os.path.join(Path.home(), "Downloads"))
-        img.save(f"{folder_path}\\{save_file_as}.png")
-    except Exception as e:
-        print(e)
+button= st.button("Select Location to Save.")
+if button:
+    st.session_state['file_location'] = get_file_path()
 
 
-
-
-
-
-
-
-
-
-
+if st.button("Click To Gen"):
+    qr_code_gen(data,st.session_state['file_location'],save_file_as)
+    st.success("File saved at: ")
 
 
