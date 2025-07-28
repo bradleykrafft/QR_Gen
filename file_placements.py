a4fa428f9bb348842fabb6ae4d0755ada9d6e68f
bradleykrafft <56617611+bradleykrafft@users.noreply.github.com>
2025-07-28 18:00:37 -0400

    Add files via upload
    change code to work with streamlit cloud

--- file_placements.py
+++ file_placements.py
@@ -1,5 +1,5 @@
 import qrcode
-import wx
+# import wx
 import PIL
 import base64
 import io
@@ -8,24 +8,24 @@ from pathlib import Path
 from PIL import Image
 
 
-def get_file_path():
-    app = wx.App(False)
-    frame = wx.Frame(None, -1, "Hidden Frame")  # Create a hidden frame for the dialog
+# def get_file_path():
+#     app = wx.App(False)
+#     frame = wx.Frame(None, -1, "Hidden Frame")  # Create a hidden frame for the dialog
+#
+#     # Create a directory dialog
+#     dialog = wx.DirDialog(frame, "Choose a directory", style=wx.DD_DEFAULT_STYLE)
+#
+#     if dialog.ShowModal() == wx.ID_OK:  # If the user selects a directory
+#         selected_directory = dialog.GetPath()  # Get the selected directory as a string
+#
+#         dialog.Destroy()  # Destroy the dialog to free resources
+#         frame.Destroy()  # Destroy the hidden frame
+#         return selected_directory
 
-    # Create a directory dialog
-    dialog = wx.DirDialog(frame, "Choose a directory", style=wx.DD_DEFAULT_STYLE)
 
-    if dialog.ShowModal() == wx.ID_OK:  # If the user selects a directory
-        selected_directory = dialog.GetPath()  # Get the selected directory as a string
-
-        dialog.Destroy()  # Destroy the dialog to free resources
-        frame.Destroy()  # Destroy the hidden frame
-        return selected_directory
-
-
-def qr_code_gen(link : str, file_path, name_of_file : str):
-    selected_dir = file_path
-    normalized_path = Path(selected_dir)
+def qr_code_gen(link : str):
+    # selected_dir = file_path
+    # normalized_path = Path(selected_dir)
     qr = qrcode.QRCode(
         version=1,
         error_correction=qrcode.constants.ERROR_CORRECT_L,
@@ -35,5 +35,9 @@ def qr_code_gen(link : str, file_path, name_of_file : str):
     qr.add_data(link)
     qr.make(fit=True)
     img = qr.make_image(fill_color="black", back_color="white")
-    img.save(f"{normalized_path}\\{name_of_file}.png")
+    byte_stream = io.BytesIO()
+    img.save(byte_stream, format='PNG')
+    byte_data = byte_stream.getvalue()
+    return byte_data
+
 
--- main.py
+++ main.py
@@ -1,21 +1,32 @@
 
 import streamlit as st
+from streamlit import session_state
+
 from file_placements import *
 
 st.title("Easy QR")
 
 data = st.text_input("Enter link to convert to QR code.")
-save_file_as = st.text_input("Enter a name for the file.")
-if 'file_location' not in st.session_state:
-    st.session_state['file_location'] = "C:\\Users\\public\\downloads"
+save_file_as = st.text_input("Enter a name for the file.") + ".png"
 
-button= st.button("Select Location to Save.")
-if button:
-    st.session_state['file_location'] = get_file_path()
+if 'generated_file' not in session_state:
+    st.session_state['generated_file'] = io.BytesIO()
 
+# if 'file_location' not in st.session_state:
+#     st.session_state['file_location'] = "C:\\Users\\public\\downloads"
+# button= st.button("Select Location to Save.")
+# if button:
+#     st.session_state['file_location'] = get_file_path()
+# if st.button("Click To Gen"):
+#     qr_code_gen(data,st.session_state['file_location'],save_file_as)
+#     st.success("File saved at: ")
+if st.button("Generate QR_Code"):
+    #img_file = st.write(qr_code_gen(data))
+    img_file = qr_code_gen(data)
 
-if st.button("Click To Gen"):
-    qr_code_gen(data,st.session_state['file_location'],save_file_as)
-    st.success("File saved at: ")
+st.download_button("Generate and save QRCode.",
+                   data=st.session_state.generated_file,
+                   file_name=save_file_as,
+                   mime="image/png")
 
 
